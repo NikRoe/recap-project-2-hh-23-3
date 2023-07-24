67f106c99a92266638a522f80cfc792b7809c2a6
Niklas Röttger <niklas.roettger@outlook.de>
2023-07-24 14:22:35 +0200

    add toggle

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // phew… not a lot going on here. Please add some code!
+
+const bookmarkButton = document.querySelector('[data-js="bookmark"]');
+const answerButton = document.querySelector('[data-js="answerButton"]');
+const answer = document.querySelector('[data-js="answer"]');
+
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toggle("bookmark--active");
+});
+
+answerButton.addEventListener("click", () => {
+  answer.classList.toggle("card__answer--active");
+});
